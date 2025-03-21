16b9163d90b83e4782c57c06b6c92c25a0214ffe
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-03-21 16:49:55 +0700

    bai2

--- Module2BaiSo1_TrinhThaoMinh/Module2BaiSo2_TrinhThaoMinh/frmBaiTap1.Designer.cs
+++ Module2BaiSo1_TrinhThaoMinh/Module2BaiSo2_TrinhThaoMinh/frmBaiTap1.Designer.cs
@@ -28,23 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnShow = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.lblYourName = new System.Windows.Forms.Label();
             this.txtYourName = new System.Windows.Forms.TextBox();
             this.lblYoB = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnShow
             // 
+            this.btnShow.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnShow.Location = new System.Drawing.Point(62, 249);
             this.btnShow.Name = "btnShow";
             this.btnShow.Size = new System.Drawing.Size(169, 76);
             this.btnShow.TabIndex = 4;
             this.btnShow.Text = "&Show";
             this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
             // 
             // btnClear
             // 
@@ -54,6 +59,7 @@
             this.btnClear.TabIndex = 5;
             this.btnClear.Text = "&Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // lblYourName
             // 
@@ -71,6 +77,8 @@
             this.txtYourName.Name = "txtYourName";
             this.txtYourName.Size = new System.Drawing.Size(436, 39);
             this.txtYourName.TabIndex = 1;
+            this.txtYourName.TextChanged += new System.EventHandler(this.txtYourName_TextChanged);
+            this.txtYourName.Leave += new System.EventHandler(this.txtYourName_Leave);
             // 
             // lblYoB
             // 
@@ -81,13 +89,14 @@
             this.lblYoB.TabIndex = 2;
             this.lblYoB.Text = "Date of birth:";
             // 
-            // textBox1
+            // txtYear
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(268, 157);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(436, 39);
-            this.textBox1.TabIndex = 3;
+            this.txtYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtYear.Location = new System.Drawing.Point(268, 157);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(436, 39);
+            this.txtYear.TabIndex = 3;
+            this.txtYear.TextChanged += new System.EventHandler(this.txtYear_TextChanged);
             // 
             // btnExit
             // 
@@ -97,6 +106,11 @@
             this.btnExit.TabIndex = 6;
             this.btnExit.Text = "&Exit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // frmBaiTap1
             // 
@@ -104,17 +118,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(781, 398);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtYear);
             this.Controls.Add(this.lblYoB);
             this.Controls.Add(this.txtYourName);
             this.Controls.Add(this.lblYourName);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnShow);
             this.Font = new System.Drawing.Font("Tahoma", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Margin = new System.Windows.Forms.Padding(7);
             this.Name = "frmBaiTap1";
             this.Text = "My Name Project";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBaiTap1_FormClosing);
             this.Load += new System.EventHandler(this.frmBaiTap1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,8 +143,9 @@
         private System.Windows.Forms.Label lblYourName;
         private System.Windows.Forms.TextBox txtYourName;
         private System.Windows.Forms.Label lblYoB;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtYear;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
