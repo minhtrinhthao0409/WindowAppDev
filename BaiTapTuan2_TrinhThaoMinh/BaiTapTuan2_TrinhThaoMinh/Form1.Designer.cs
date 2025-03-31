bfec3a84ee328184dbc75495bbc724d9ab05baed
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-03-31 15:32:08 +0700

    BT tuần 2

---
+++ BaiTapTuan2_TrinhThaoMinh/BaiTapTuan2_TrinhThaoMinh/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BaiTapTuan2_TrinhThaoMinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadFolder = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFolder
+            // 
+            this.btnLoadFolder.Location = new System.Drawing.Point(49, 30);
+            this.btnLoadFolder.Name = "btnLoadFolder";
+            this.btnLoadFolder.Size = new System.Drawing.Size(90, 30);
+            this.btnLoadFolder.TabIndex = 0;
+            this.btnLoadFolder.Text = "&Load Folder";
+            this.btnLoadFolder.UseVisualStyleBackColor = true;
+            this.btnLoadFolder.Click += new System.EventHandler(this.btnLoadFolder_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(49, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 263);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(52, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(701, 42);
+            this.label1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(493, 66);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(260, 263);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFolder;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ BaiTapTuan2_TrinhThaoMinh/BaiTapTuan2_TrinhThaoMinh/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapTuan2_TrinhThaoMinh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            var res = dialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+                string[] files = Directory.GetFiles(path);
+                foreach ( string file in files )
+                {
+                    if (file.EndsWith(".jpg") || file.EndsWith(".png"))
+                    {
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Cursor = Cursors.Hand;
+                        pictureBox.Load(file);
+                        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pictureBox.Tag = file;
+
+                        pictureBox.Click += PictureBox_Click;
+
+                        flowLayoutPanel1.Controls.Add(pictureBox);
+                    }
+                }
+            }
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            if (sender is PictureBox pictureBox)
+            {
+                string imgPath = pictureBox.Tag.ToString();
+                pictureBox1.Load(imgPath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                label1.Text = "File" + imgPath + " is loaded.";
+            }
+        }
+    }
+}
