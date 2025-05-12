e39f45bbccb96bb0cf4a1477ff03e8a4067de3df
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-05-12 11:40:06 +0700

    ok

--- StudentManagement_/Program.cs
+++ StudentManagement_/Program.cs
@@ -5,10 +5,58 @@ namespace StudentManagement_
     {
         static void Main(string[] args)
         {
-            using var db = new StudentDbContext();
+            using var context = new StudentDbContext();
 
-            Console.WriteLine($"Database path: {db.DbPath}.");
+            var students = context.Students
+                .Select(s => new
+                {
+                    s.Name,
+                    Homeworks = s.HomeworkSubmissions.Select(h => new
+                    {
+                        h.Content,
+                        h.ContentType
+                    })
+                });
 
-        }
-    }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Student: {student.Name}");
+                foreach (var hw in student.Homeworks)
+                {
+                    Console.WriteLine($"\tHomework: {hw.Content}, Type: {hw.ContentType}");
+                }
+            }
+
+            var courses = context.Courses
+            .Include(c => c.Resources)
+            .OrderBy(c => c.StartDate)
+            .ThenByDescending(c => c.EndDate);
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"Course: {course.Name} - {course.Description}");
+                foreach (var res in course.Resources)
+                {
+                    Console.WriteLine($"\tResource: {res.Name} ({res.Type}) - {res.Url}");
+                }
+            }
+
+            var courses_ = context.Courses
+            .Where(c => c.Resources.Count > 5)
+            .Select(c => new
+            {
+                c.Name,
+                ResourceCount = c.Resources.Count
+            })
+            .OrderByDescending(c => c.ResourceCount)
+            .ThenByDescending(c => c.Name); // hoặc StartDate nếu cần
+
+                    foreach (var course in courses_)
+                    {
+                        Console.WriteLine($"Course: {course.Name}, Resources: {course.ResourceCount}");
+                    }
+
+                }
+            }
 }
